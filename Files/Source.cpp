48044a050facf05b3e8002882ec7ece4bc2f937a
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2023-02-20 21:21:07 +0300

    file done 2

--- Files/Source.cpp
+++ Files/Source.cpp
@@ -2,19 +2,51 @@
 #include<fstream>
 using namespace std;
 
-//#define WRITE_TO_FILE
+#define WRITE_TO_FILE
+//#define READ_FROM_FILE
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
 #ifdef WRITE_TO_FILE
+	char filename[_MAX_FNAME] = {};
+	cout << "Ввeдите имя файла : ";
+	cin.getline(filename, _MAX_FNAME);
+	if (strcmp(filename + strlen(filename) - 4, ".txt"))strcat_s(filename,_MAX_FNAME, ".txt");
+
 	ofstream fout;                   //1) Создаем поток
-	fout.open("File.txt", std::ios_base::app);  //2) Открываем поток
+	fout.open(filename, std::ios_base::app);  //2) Открываем поток
 	fout << "Hello files" << endl;   //3) Пишем в поток
 	fout.close();                    //4) Закрываем поток
-	system("notepad File.txt");
+
+	char sz_command[_MAX_FNAME] = "notepad ";
+	strcat_s(sz_command,_MAX_FNAME, filename);
+	system(sz_command);
 #endif // WRITE_TO_FILE
 
+#ifdef READ_FROM_FILE
+	ifstream fin;
+	fin.open("File.txt");
+	if (fin.is_open())
+	{
+		const int SIZE = 1024 * 100;
+		char sz_buffer[SIZE]{};
+		while (!fin.eof())
+		{
+			//fin >> sz_buffer;
+			fin.getline(sz_buffer, SIZE);
+			cout << sz_buffer << endl;
+		}
+		fin.close();
+	}
+	else
+	{
+		std::cerr << "Error: File not found" << endl;
+		//cout - console out
+		//cerr - console error
+
+	}
+#endif // READ_FROM_FILE
 
 }
