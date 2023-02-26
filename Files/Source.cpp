3f26a0a46f86876b690532d28596ef92a8764d2c
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2023-02-26 21:25:33 +0300

    Files done

--- Files/Source.cpp
+++ Files/Source.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 using namespace std;
 
-#define WRITE_TO_FILE
+//#define WRITE_TO_FILE
 //#define READ_FROM_FILE
 
 void main()
@@ -30,7 +30,7 @@ void main()
 	fin.open("File.txt");
 	if (fin.is_open())
 	{
-		const int SIZE = 1024 * 100;
+		const int SIZE = 1256;
 		char sz_buffer[SIZE]{};
 		while (!fin.eof())
 		{
@@ -49,4 +49,48 @@ void main()
 	}
 #endif // READ_FROM_FILE
 
+
+
+	char filename[_MAX_FNAME] = {};
+	//cin.getline(filename, _MAX_FNAME);
+	if (strcmp(filename + strlen(filename) - 4, "201 ready.txt"))
+		strcat_s(filename, _MAX_FNAME, "201 ready.txt");
+
+	ofstream fout("201 ready.txt");
+	
+	//fout.open(filename, std::ios_base::app);
+	fout.close();
+
+	char sz_command[_MAX_FNAME] = "notepad ";
+	strcat_s(sz_command, _MAX_FNAME, filename);
+	system(sz_command);
+
+	ifstream fin;
+	fin.open("201 RAW.txt");
+	if (fin.is_open())
+	{
+		const int IP = 16;
+		char IP_buffer[IP]{};
+		for (int i = 0; i < IP; i++)
+		{
+			fin >> IP_buffer[i];
+		}
+		const int MAC = 18;
+		char MAC_buffer[MAC]{};
+		for(int i=0; i<MAC; i++)
+		{
+			fin >> MAC_buffer[i];
+		}
+		
+		while (!fin.eof())
+		{
+			fout << MAC << " " << IP << endl;
+		}
+		fin.close();
+	}
+	else
+	{
+		std::cerr << "Error: File not found" << endl;
+		
+	}
 }
